67d6557de9e8c512705369a57c8ee480db9e1b1b
v.adhithyan <aavispeaks@gmail.com>
2016-11-06 09:07:24 +0530

    comments added and readme updated

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # price-fluctuations-of-petrol
 
-  Price list was scraped from http://www.mypetrolprice.com/5/Petrol-price-in-Chennai and stored in csv format
+  Price list was scraped from http://www.mypetrolprice.com/5/Petrol-price-in-Chennai and stored in csv format. In this assignment I considered only Chennai.
+
+# Requirements
+- Python 2.7 - to scrape prices and year into csv
+- Elasticsearch - to index csv data for visualization
+- Kibana - to visualize data (see the below pic)
 
 ## Visualization of petrol rates from 2002 to 2016
 ![sd of petrol rates from 2002 to 2016](https://raw.githubusercontent.com/v-adhithyan/petrol-price-fluctuation/master/stats/petrol-rates-sd-visual.png)
@@ -9,14 +14,15 @@
 
 * Increase in crude oil rate, increases petrol price.
 
-## Findings
+## Sources
 
 * https://www.google.co.in/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=petrol%20rates%20steep%20increase%20from%202010%20to%202012
 * http://indiatoday.intoday.in/story/petrol-price-hikes-in-the-last-two-years/1/192328.html
 * http://business.mapsofindia.com/india-petroleum-industry/petrol-price-hike/
-
-# links
-
 * http://www.eia.gov/energyexplained/index.cfm?page=gasoline_fluctuations
 * http://www.johnhughes.com.au/blog/prices-of-petrol-gas-fuel-prices-how-and-why-they-fluctuate/
 * http://www.eia.gov/energyexplained/index.cfm?page=gasoline_factors_affecting_prices
+* https://www.quora.com/Why-are-petrol-prices-rising-in-India
+
+## License
+MIT.
--- scraper.py
+++ scraper.py
@@ -3,36 +3,39 @@ import time
 import os
 import csv
 
-to_csv = []
-
-def chrome_driver():
-        driver = os.getcwd() + os.path.sep + "driver" + os.path.sep + "chromedriver"
-        #print driver
-        os.environ["webdriver.chrome.driver"] = driver
-        return driver
-
-url = "http://www.mypetrolprice.com/5/Petrol-price-in-Chennai"
-chrome = webdriver.Chrome(chrome_driver())
-
-chrome.get(url)
-
 def pretty_print():
     print "****************************************"
 
-
+""" function to write scraped data to csv file """
 def write_to_csv(content):
     with open("petrol-rates.csv", "wb") as csvfile:
         writer = csv.DictWriter(csvfile, content[0].keys())
         writer.writeheader()
         writer.writerows(content)
 
+""" function returns selenium mac driver """
+def chrome_driver():
+        driver = os.getcwd() + os.path.sep + "driver" + os.path.sep + "chromedriver"
+        #print driver
+        os.environ["webdriver.chrome.driver"] = driver
+        return driver
+
+to_csv = []
+
+url = "http://www.mypetrolprice.com/5/Petrol-price-in-Chennai"
+chrome = webdriver.Chrome(chrome_driver())
+chrome.get(url)
 
 for i in range(30):
     print "page #{}".format(i+1)
     pretty_print()
 
-
+    """
+    after inspecting source code of http://www.mypetrolprice.com/5/Petrol-price-in-Chennai
+    it is found that:
+    span element with class name price contains price. find and extract """
     prices = chrome.find_elements_by_css_selector("span[class='price']")
+    """span element with class name alignment contains date. find and extract """
     dates = chrome.find_elements_by_css_selector("span[class='alignment']")
 
     for i, val in enumerate(prices):
